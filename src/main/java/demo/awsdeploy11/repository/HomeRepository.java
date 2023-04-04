01d8b4cb58e86dbc7c6cf0cc99911fd53c5a0bb8
Hemant <hjangid125@gmail.com>
2023-04-04 12:41:45 +0530

    Only Hello World

--- src/main/java/demo/awsdeploy11/repository/HomeRepository.java
+++
@@ -1,7 +0,0 @@
-package demo.awsdeploy11.repository;
-
-import demo.awsdeploy11.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HomeRepository extends JpaRepository<Student, Integer> {
-}
